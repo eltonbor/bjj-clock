ef118bd7517b82f6f974bd1f0674e0726014f30b
Elton Tasca Borssoi <eltonbor@gmail.com>
2023-09-04 19:49:50 -0400

    Add beep sound generator

---
+++ beep.js
@@ -0,0 +1,25 @@
+function longBeep() {
+  var context = new AudioContext();
+  var oscillator = context.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = 800;
+  oscillator.connect(context.destination);
+  oscillator.start(); 
+  // Beep for 1000 milliseconds
+  setTimeout(function () {
+    oscillator.stop();
+  }, 1000);
+}
+
+function shortBeep() {
+  var context = new AudioContext();
+  var oscillator = context.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = 800;
+  oscillator.connect(context.destination);
+  oscillator.start(); 
+  // Beep for 100 milliseconds
+  setTimeout(function () {
+    oscillator.stop();
+  }, 100);
+}
